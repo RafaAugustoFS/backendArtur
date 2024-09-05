a2ba1f2704bbb495a78af2ce787197e615a2aa37
RafaAugustoFS <rafaelaugustofsilva@gmail.com>
2024-09-05 16:36:00 -0300

    creating a upload system

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken')
 const userRoutes = require("./routerUser")
 const productRoutes = require("./routesProduct")
 const colaboratorRoutes = require("./routesColaborator");
+const uploadRoutes =require('./routerUpload');
 const UserController = require("../controller/userController");
 
 const router = Router();
 
-
-
+router.use('/image', uploadRoutes);
 router.use('/user', userRoutes);
 router.use('/product', productRoutes);
 router.use('/colaborator', colaboratorRoutes);
--- src/server.js
+++ src/server.js
@@ -1,36 +1,46 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = require("./router/router");
-const sequelize = require('./config/config');
-const User = require("./models/user")
-const Product = require("./models/product")
+const sequelize = require("./config/config");
+const cors = require("cors");
+const User = require("./models/user");
+const Product = require("./models/product");
 
 const app = express();
 //API modelo JSON
 app.use(express.json());
 
-app.use('/api/', router);
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); //
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-app.get('/healthcheck', (req,res) =>{
-    // 200 -> OK
-    return res.status(200).json({
-        msg: 'Estamos vivos!',
-        alive: true
-    })
-})
+app.use("/api/", router);
 
-sequelize.authenticate()
-.then(async () =>{
+app.get("/healthcheck", (req, res) => {
+  // 200 -> OK
+  return res.status(200).json({
+    msg: "Estamos vivos!",
+    alive: true,
+  });
+});
+
+sequelize
+  .authenticate()
+  .then(async () => {
     console.log("Conexão estabelecida com sucesso!");
-    await sequelize.sync({ }); //Sincroniza o código com a tabela
-})
-.then(() =>{
-    app.listen(process.env.PORT == null ? 8080 : process.env.PORT, () =>{
-        console.log("#####");
-        console.log("Rodando na porta 8080");
-        console.log("#####");
-    })
-})
-.catch(() =>{
-    console.error( "Erro ao se conectar com o DataBase!");
-})
+    await sequelize.sync({}); //Sincroniza o código com a tabela
+  })
+  .then(() => {
+    app.listen(process.env.PORT == null ? 8080 : process.env.PORT, () => {
+      console.log("#####");
+      console.log("Rodando na porta 8080");
+      console.log("#####");
+    });
+  })
+  .catch(() => {
+    console.error("Erro ao se conectar com o DataBase!");
+  });
